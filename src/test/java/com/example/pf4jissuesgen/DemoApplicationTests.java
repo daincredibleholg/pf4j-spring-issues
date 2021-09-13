300549ccf136de1f16ac8953a7d4cbce4ae2e6e6
hs554n <hsteinhauer@wayfair.com>
2021-09-13 17:25:20 +0200

    Initial commit

---
+++ src/test/java/com/example/pf4jissuesgen/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pf4jissuesgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
